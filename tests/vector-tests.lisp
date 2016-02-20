9ee6fe292f7ba25855a1cad996625ed0edd70574
mickbeaver <m.charles.beaver@gmail.com>
2016-02-19 22:14:54 -0600

    Added another cross-product test.

--- tests/vector-tests.lisp
+++ tests/vector-tests.lisp
@@ -3,7 +3,8 @@
 (defconstant +float-epsilon+ 0.000001)
 
 (defun float-approx-equal (float1 float2)
-  (<= +float-epsilon+ (abs (- float1 float2))))
+  (<= (abs (- float1 float2))
+      +float-epsilon+))
 
 (defun vec3-approx-equal (a b)
   (and (float-approx-equal (vec-x a)
@@ -16,5 +17,7 @@
 (define-test test-cross-product
     (assert-true (vec3-approx-equal (vec3-cross (make-vec3 1.0 0.0 0.0)
                                                 (make-vec3 0.0 1.0 0.0))
-                                    (make-vec3 0.0 0.0 1.0))))
-                             
+                                    (make-vec3 0.0 0.0 1.0)))
+    (assert-true (vec3-approx-equal (vec3-cross (make-vec3 2.0  2.0 1.0)
+                                                (make-vec3 1.0 -2.0 0.0))
+                                    (make-vec3 2.0 1.0 -6.0))))
