b8934d849e1391eb863caa00c5c103c03969a856
mickbeaver <m.charles.beaver@gmail.com>
2016-02-20 21:21:52 -0600

    Adding more vector functions and tests. Tests are broken, though.

--- tests/vector-tests.lisp
+++ tests/vector-tests.lisp
@@ -6,6 +6,12 @@
   (<= (abs (- float1 float2))
       +float-epsilon+))
 
+(defun vec2-approx-equal (a b)
+  (and (float-approx-equal (vec-x a)
+                           (vec-x b))
+       (float-approx-equal (vec-y a)
+                           (vec-y b))))
+
 (defun vec3-approx-equal (a b)
   (and (float-approx-equal (vec-x a)
                            (vec-x b))
@@ -13,6 +19,11 @@
                            (vec-y b))
        (float-approx-equal (vec-z a)
                            (vec-z b))))
+
+(define-test test-vec2-add
+    (assert-true (vec2-approx-equal (vec2-add (make-vec2 1.0 1.0)
+                                              (make-vec2 1.0 1.0))
+                                    (make-vec2 2.0 2.0))))
 
 (define-test test-cross-product
     (assert-true (vec3-approx-equal (vec3-cross (make-vec3 1.0 0.0 0.0)
