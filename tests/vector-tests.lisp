49dd308aebcf372a65f366186a4e3453e02d40df
Mick Beaver <m.charles.beaver@gmail.com>
2016-02-24 08:39:06 -0600

    vec2 tests.

--- tests/vector-tests.lisp
+++ tests/vector-tests.lisp
@@ -21,9 +21,9 @@
                            (vec-z b))))
 
 (define-test test-vec2-add-01
-    (assert-true (vec2-approx-equal (vec2-add (make-vec2 1.0 1.0)
-                                              (make-vec2 1.0 1.0))
-                                    (make-vec2 2.0 2.0))))
+  (assert-true (vec2-approx-equal (vec2-add (make-vec2 1.0 1.0)
+                                            (make-vec2 1.0 1.0))
+                                  (make-vec2 2.0 2.0))))
 
 (define-test test-vec2-distance-01
   (assert-true (float-approx-equal (vec2-distance (make-vec2 10.0 6.0)
@@ -43,14 +43,34 @@
 (define-test test-vec2-length-01
   (assert-true (float-approx-equal (vec2-length (make-vec2 -12.0 5.0))
                                    13.0)))
-  
+
 (define-test test-vec2-normalize-01
-    (assert-true (vec2-approx-equal (vec2-normalize (make-vec2 12.0 5.0))
-                                    (make-vec2 0.923077 0.384615))))
+  (assert-true (vec2-approx-equal (vec2-normalize (make-vec2 12.0 5.0))
+                                  (make-vec2 0.923077 0.384615))))
 
 (define-test test-vec2-normalize-02
-    (assert-true (vec2-approx-equal (vec2-normalize (make-vec2 0.0 743.632))
-                                    (make-vec2 0.0 1.0))))
+  (assert-true (vec2-approx-equal (vec2-normalize (make-vec2 0.0 743.632))
+                                  (make-vec2 0.0 1.0))))
+
+(define-test test-vec2-projection-01
+  (assert-true (vec2-approx-equal (vec2-projection (make-vec2 4.0 3.0)
+                                                   (make-vec2 (/ (sqrt 2.0) 2.0)
+                                                              (/ (sqrt 2.0) 2.0)))
+                                  (make-vec2 3.5 3.5))))
+
+(define-test test-vec2-scale-01
+  (assert-true (vec2-approx-equal (vec2-scale 3.0 (make-vec2 4.0 -7.0))
+                                  (make-vec2 12.0 -21.0))))
+
+(define-test test-vec2-sub-01
+  (assert-true (vec2-approx-equal (vec2-sub (make-vec2 3.0 10.0)
+                                            (make-vec2 8.0 -7.0))
+                                  (make-vec2 -5.0 17.0))))
+
+(define-test test-vec2-sub-02
+  (assert-true (vec2-approx-equal (vec2-sub (make-vec2 4.0 5.0)
+                                            (make-vec2 -4.0 -5.0))
+                                  (make-vec2 8.0 10.0))))
 
 ;; (define-test test-cross-product
 ;;     (assert-true (vec3-approx-equal (vec3-cross (make-vec3 1.0 0.0 0.0)
